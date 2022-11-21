d3ecd58f83b4b31d5c9927e4fed6a7036d87b22b
jesbor98 <112757660+jesbor98@users.noreply.github.com>
2022-11-21 20:33:10 +0100

    mainfunktion, uppdateringsloop/renderingsloop, space_rect upp/ner/höger/vänster, börja på jump

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,23 @@
 #include <SDL2/SDL_mixer.h> 
 #include <SDL2/SDL_ttf.h>
 
+void jump(SDL_Rect& rect, SDL_Renderer& ren) {
+bool jumping = true;
+int orginalheight = rect.x;
+int height = rect.x + 50;
+while(jumping) {
+  if(rect.x < height) {
+    rect.x+=10;
+  }
+  SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, bg_tex, NULL, NULL);
+    SDL_RenderCopy(renderer, spacepig_tex, NULL, &spacepig_rect);
+    SDL_RenderPresent(renderer);
+}
+jumping = false;
+
+}
+
 int main(int argc, char* argv[]) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) 
 	{
@@ -26,6 +43,10 @@ int main(int argc, char* argv[]) {
 	SDL_Surface* bg_sur = IMG_Load( (constants::gResPath + "images/bg.jpg").c_str() );
 	SDL_Surface* spacepig_sur = IMG_Load( (constants::gResPath + "images/spacepig2.png").c_str() );
 
+  SDL_Rect spacepig_rect = {100, 100, (spacepig_sur->w)/2, (spacepig_sur->h)/2};
+  Uint32 white = SDL_MapRGB(spacepig_sur->format, 255, 255, 255);
+  SDL_SetColorKey(spacepig_sur, true, white);
+ 
 	SDL_Texture* bg_tex = SDL_CreateTextureFromSurface(renderer, bg_sur);
 	SDL_Texture* spacepig_tex = SDL_CreateTextureFromSurface(renderer, spacepig_sur);
 	SDL_FreeSurface(bg_sur);
@@ -35,17 +56,38 @@ int main(int argc, char* argv[]) {
 
 	// Loop till dess att programmet avslutas!
 	bool running = true;
+  //Sprite player{10, 10, "Amanda"};
 	while (running) {
-		SDL_Event e;
-		if (SDL_PollEvent(&e)) {
-			if (e.type == SDL_QUIT) {
-				running = false;
-			}
-		}
+		SDL_Event event;
+    while(SDL_PollEvent(&event)){
+      switch (event.type) {
+        case SDL_QUIT: running = false; break;
+        case SDL_KEYDOWN: 
+          switch(event.key.keysym.sym) {
+            case SDLK_RIGHT: spacepig_rect.x+=10; break;
+            case SDLK_LEFT: spacepig_rect.x-=10; break;
+            //case SDLK_UP:spacepig_rect.y-=10; break;
+            //case SDLK_DOWN:spacepig_rect.y+=10; break;
+            case SDLK_SPACE: jump(spacepig_rect, renderer);
+          } break;
+      if (event.key.keysym.sym == SDLK_END)
+      running = true;
+      break;
+      } // switch
+    }
+
+
+    
+		// if (SDL_PollEvent(&event)) {
+		// 	if (event.type == SDL_QUIT) {
+		// 		running = false;
+		// 	}
+		// }
+    
 
 		SDL_RenderClear(renderer);
 		SDL_RenderCopy(renderer, bg_tex, NULL, NULL);
-		SDL_RenderCopy(renderer, spacepig_tex, NULL, NULL);
+		SDL_RenderCopy(renderer, spacepig_tex, NULL, &spacepig_rect);
 		SDL_RenderPresent(renderer);
 	}
 
