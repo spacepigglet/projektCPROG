a4fa90beff0a69ea63dd68dac3b05e8080e372b7
jesbor98 <112757660+jesbor98@users.noreply.github.com>
2022-12-20 12:19:30 +0100

    changed errors in immobile h and cpp

--- include/ImmobileComponent.h
+++ include/ImmobileComponent.h
@@ -15,4 +15,4 @@ class ImmobileComponent : public Component {
 
 };
 }
-#endif;
+#endif
--- src/ImmobileComponent.cpp
+++ src/ImmobileComponent.cpp
@@ -2,13 +2,10 @@
 
 namespace tower {
 
-class ImmobileComponent : public Component {
 
   ImmobileComponent::ImmobileComponent(int x, int y, int w, int h) : Component(x,y,w,h) {}
 
 
   ImmobileComponent::~ImmobileComponent() {}
-
-};
 
 }
