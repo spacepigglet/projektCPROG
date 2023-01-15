5a6eb271e68dd7c6be47934d7729dc8a57be309d
spacepigglet <97839498+spacepigglet@users.noreply.github.com>
2023-01-15 12:35:34 +0100

    ImmobileComponent new purpose, used to put still images on screen - heart!

--- include/ImmobileComponent.h
+++ include/ImmobileComponent.h
@@ -2,16 +2,26 @@
 #define IMMOBILECOMPONENT_H
 
 #include "Component.h"
+#include <string>
+#include "Constants.h"
+#include <SDL2/SDL_image.h>
+#include "System.h"
 
 namespace tower {
 class ImmobileComponent : public Component {
 
-  /* public:
-  virtual void draw() const = 0;
+   public:
+  virtual void draw() const;
   ~ImmobileComponent();
+  static ImmobileComponent* getInstance(int, int, int, int, std::string);
+  static ImmobileComponent* getInstance(int, int, int, int, std::string, bool);
 
   protected:
-  ImmobileComponent(int, int, int, int); */
+  ImmobileComponent(int, int, int, int, std::string ); 
+  ImmobileComponent(int, int, int, int, std::string, bool); 
+
+  private:
+  SDL_Texture* texture;
 
 };
 }
--- src/ImmobileComponent.cpp
+++ src/ImmobileComponent.cpp
@@ -3,9 +3,27 @@
 namespace tower {
 
 
-  /* ImmobileComponent::ImmobileComponent(int x, int y, int w, int h) : Component(x,y,w,h) {}
+  ImmobileComponent::ImmobileComponent(int x, int y, int w, int h, std::string img, bool scrollable) : Component(x,y,w,h,scrollable) {
+    texture = IMG_LoadTexture(sys.get_ren(), (constants::gResPath + img).c_str() );
+  }
+  ImmobileComponent::ImmobileComponent(int x, int y, int w, int h, std::string img) : Component(x,y,w,h) {
+    ImmobileComponent(x,y,w,h,img,true);
+  }
 
+  ImmobileComponent* ImmobileComponent::getInstance(int x, int y, int w, int h, std::string img){
+    return new ImmobileComponent(x, y, w, h, img);
+  }
 
-  ImmobileComponent::~ImmobileComponent() {} */
+  ImmobileComponent* ImmobileComponent::getInstance(int x, int y, int w, int h, std::string img, bool scrollable){
+    return new ImmobileComponent(x, y, w, h, img, scrollable);
+  }
+
+  void ImmobileComponent::draw() const{
+    SDL_RenderCopy(sys.get_ren(), texture, NULL, &getRect());
+  }
+
+  ImmobileComponent::~ImmobileComponent() {
+    SDL_DestroyTexture(texture);
+  } 
 
 }
